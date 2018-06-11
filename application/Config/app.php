18691bc8b2355677c35c55aec539c45f3ac451b7
Andrey Vitsentovich <vitsentovich.andrey@gmail.com>
2018-06-11 17:41:38 +0300

    Fix URL const in config.

--- application/Config/app.php
+++ application/Config/app.php
@@ -4,7 +4,7 @@
  * Configuration for: URL and paths
  */
 define('PUBLIC_FOLDER_PATH', ROOT . 'public' . DIRECTORY_SEPARATOR);
-define('URL', $_SERVER['HTTP_HOST']);
+define('URL', '//' . $_SERVER['HTTP_HOST']);
 define('VIEWS_PATH', APP . 'View' . DIRECTORY_SEPARATOR);
 define('LAYOUT_PATH', VIEWS_PATH . 'layouts/app.php');
 
