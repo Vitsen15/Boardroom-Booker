4a68f01a3491ce666f0693012b9e3458b3918d5b
Andrey Vitsentovich <vitsentovich.andrey@gmail.com>
2018-06-12 12:16:38 +0300

    Fix session loosing.

--- application/Core/Controller.php
+++ application/Core/Controller.php
@@ -15,6 +15,8 @@ abstract class Controller
 
     protected function checkAuth()
     {
+        session_start();
+
         if (
             !isset($_SESSION['accessToken']) ||
             !isset($_COOKIE['accessToken']) ||
