3f28abbf18cf7df6540fdb53698bd7a004db0176
Andrey Vitsentovich <vitsentovich.andrey@gmail.com>
2018-06-13 17:59:40 +0300

    Add description for FIRST_DAY_OF_WEEK config, add HOURS_FORMAT config.

--- application/Config/calendar.php
+++ application/Config/calendar.php
@@ -4,5 +4,16 @@
  * Configuration for: Calendar
  */
 define('BOARDROOMS_COUNT', 3);
+
+// Day of week configuration example
+//define('FIRST_DAY_OF_WEEK', 'sunday');
 //define('FIRST_DAY_OF_WEEK', 'monday');
+
 define('FIRST_DAY_OF_WEEK', 'sunday');
+
+// Hours format displaying configuration example
+//
+//define('HOURS_FORMAT', 12);
+//define('HOURS_FORMAT', 24);
+
+define('HOURS_FORMAT', 24);
