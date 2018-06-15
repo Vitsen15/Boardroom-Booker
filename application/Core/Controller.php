17ed73c3694f98aecbd54f26c6d650102bfee826
Andrey Vitsentovich <vitsentovich.andrey@gmail.com>
2018-06-15 19:16:31 +0300

    Expand views rendering logic, to render view without template.

--- application/Core/Controller.php
+++ application/Core/Controller.php
@@ -7,18 +7,23 @@ abstract class Controller
     protected $model;
 
     /**
-     * Renders provided view inside layout.
+     * Renders provided view inside layout or standalone.
      *
      * @param string $template - Path to view
      * @param array $data
+     * @param bool $renderWithLayout
      * @return string
      */
-    protected function view($template, $data = null)
+    protected function view($template, $data = null, $renderWithLayout = true)
     {
         if (isset($data)) {
             extract($data);
         }
 
-        return include LAYOUT_PATH;
+        if ($renderWithLayout){
+            return include LAYOUT_PATH;
+        } else {
+            include $template;
+        }
     }
 }
