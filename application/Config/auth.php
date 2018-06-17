9dcfc1b2485e80bc11f82daa8e9cfa110f17fd5d
Andrey Vitsentovich <vitsentovich.andrey@gmail.com>
2018-06-17 12:06:07 +0300

    Delete auth.php.sample and add auth.php.

---
+++ application/Config/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Configuration for: Authorization
+ */
+define('SALT', 'kdjh34jk89j7298js7d9238798');
+define('USERNAME_MIN_LENGTH', 6);
+define('USERNAME_MAX_LENGTH', 15);
+define('PASSWORD_MIN_LENGTH', 8);
+define('PASSWORD_MAX_LENGTH', 18);
